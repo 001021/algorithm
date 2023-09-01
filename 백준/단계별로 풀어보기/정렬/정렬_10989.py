9179a768d89d973ad621fbe766d42c6a92c87dbc
Replit user <>
2023-09-01 05:45:03 +0000

    BOJ #1427: 소트인사이드

--- 백준/단계별로 풀어보기/정렬/정렬_10989.py
+++ 백준/단계별로 풀어보기/정렬/정렬_10989.py
@@ -1,16 +1,13 @@
-def counting_sort(arr):
-  # 입력 배열 전체를 스캔해서 최대값 K를 찾는다.
-  max_arr = max(arr)
+import sys
 
-  # 카운팅 배열을 생성한다.
-  count = [0] * (max_arr + 1)
-  sorted_arr = list()
+n = int(sys.stdin.readline())
+count = [0] * 10001
 
-  # 입력 배열의 값을 기준으로 원소 개수를 업데이트한다.
-  for i in arr:
-    count[i] += 1
+for _ in range(n):
+  num = int(sys.stdin.readline())
+  count[num] += 1
 
-  # 카운팅 배열을 정렬 배열로 바꿔준다.
-  for i in range(max_arr + 1):
+for i in range(10001):
+  if count[i] != 0:
     for _ in range(count[i]):
-      sorted_arr.append(i)
+      print(i)
